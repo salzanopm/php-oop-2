95904dbc7c867032c6eaa7ffab6e319eb877ec5d
salzanopm <salzanopm@gmail.com>
2022-02-01 14:39:56 +0100

    adj

--- Smartphone.php
+++ Smartphone.php
@@ -1,6 +1,8 @@
 <?php
     class Smartphone {
 
+        use Warranty;
+
         public $marca;
 
         public $modello;
---
+++ Warranty.php
@@ -0,0 +1,11 @@
+<?php
+
+    trait Warranty {
+
+        public $warranty_code;
+
+        public $warranty_type;
+
+    }
+
+?>
